721fbc2c4a3de9b6b616183ca16b19316bf7bcf6
Ilesh_iOS <ios@siliconithub.in>
2019-03-12 14:38:02 +0530

    - Framework of stickerview application.
    - Framework of stickerview application.

---
+++ StickerView/StickerView.h
@@ -0,0 +1,19 @@
+//
+//  StickerView.h
+//  StickerView
+//
+//  Created by macmini on 05/02/19.
+//  Copyright © 2019 IP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StickerView.
+FOUNDATION_EXPORT double StickerViewVersionNumber;
+
+//! Project version string for StickerView.
+FOUNDATION_EXPORT const unsigned char StickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StickerView/PublicHeader.h>
+
+
